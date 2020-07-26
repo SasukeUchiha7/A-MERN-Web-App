57321cc965ce571d4db14e0c3e0fb292c93958be
SasukeUchiha7 <apil07cr@gmail.com>
2020-07-27 02:24:42 +0530

    homepage

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Home extends Component {
   state = {};
   render() {
-    return <div>This is the Home page</div>;
+    return (
+      <div class="jumbotron">
+        <h1 class="display-4">Hello!!</h1>
+        <p class="lead">This is a home page for all users.</p>
+        <hr class="my-4" />
+        <p>sdsfdsgdfgfdgdfasd.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">
+          Learn more
+        </a>
+      </div>
+    );
   }
 }
 
