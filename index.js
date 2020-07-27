87e1f63d2077733a01709875f73970647f8b4804
SasukeUchiha7 <apil07cr@gmail.com>
2020-07-27 19:03:44 +0530

    get post api

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+mongoose
+  .connect(require("./configs/key").MONGO_URI)
+  .then(() => {
+    console.log("Connected to the databse");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/jobs", require("./routes/jobs"));
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const Job = require("../models/job");
 
 const router = express.Router();
 
-
-// get -api "/jobs"
+// to get jobs -api- "/jobs"
 router.get("/", (req, res) => {
-  res.send("Hey this is jobs api");
+  Job.find((err, job) => {
+    if (err) {
+      res.json({ msg: "Error retrieving jobs" });
+    } else {
+      res.json(job);
+    }
+  });
+});
+
+//to post jobs -api- "/jobs"
+router.post("/", (req, res) => {
+  const newjob = new Job({
+    jobname: req.body.jobname,
+    jobdesc: req.body.jobdesc,
+  });
+  newjob.save((err) => {
+    if (err) {
+      res.json({ msg: "Failed to add job" });
+    } else {
+      res.json({ msg: "Job added successfully" });
+    }
+  });
 });
 
 module.exports = router;
